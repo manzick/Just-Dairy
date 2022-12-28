6592ddebe2e31dae45862589cba3c5286f6ca3c5
manzick <manzurov.alex@yahoo.com>
2022-12-28 20:58:07 +0600

    Initial Commit

---
+++ Just Diary/Just_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_DiaryApp.swift
+//  Just Diary
+//
+//  Created by Alexander Manzurov on 28.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Just_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
